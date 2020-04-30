a6876a1ff715cd699db5f6ee9aee744d0ed0f713
dinhducbac <63286941+dinhducbac@users.noreply.github.com>
2020-05-01 00:21:16 +0700

    Add player move, add 1 osbstacle

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    [SerializeField] float Speed = 0.8f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class Obstacle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector2(transform.position.x - Speed * Time.deltaTime, transform.position.y);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
+
 
 public class Player : MonoBehaviour
 {
@@ -13,6 +15,43 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+    void Move()
+    {
+        PCInput2();
+    }
+    void PCInput2()
+    {
+        int y = (Input.GetAxis("Vertical")  > 0 )?1: (Input.GetAxis("Vertical") < 0)?-1:0;
+        transform.position = new Vector2(transform.position.x, y);
+    }
+    void PCInput()
+    {
+        if (transform.position.y == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                transform.position = new Vector2(transform.position.x, 1);
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                transform.position = new Vector2(transform.position.x, -1);
+            }
+        }
+        if (transform.position.y == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                transform.position = new Vector2(transform.position.x, 0);
+            }
+        }
+        if (transform.position.y == -1)
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                transform.position = new Vector2(transform.position.x, 0);
+            }
+        }
     }
 }
