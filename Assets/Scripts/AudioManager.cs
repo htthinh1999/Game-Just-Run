86177b27bc61b672e3e4e52f4f5cacf91ce304e2
htthinh1999 <htthinh1999@gmail.com>
2020-04-30 23:26:58 +0700

    Update audio manager script

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,17 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+}
+
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Sound[] sound;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
