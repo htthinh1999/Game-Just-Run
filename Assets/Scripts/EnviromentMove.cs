bb698b16aeabe21dca1d7a3fbd47e6ef2a8eec96
htthinh1999 <htthinh1999@gmail.com>
2020-05-03 00:34:51 +0700

    Add enviroment to Background Scene and enviroment move script. Import Ferr2D Terrain Tool

---
+++ Assets/Scripts/EnviromentMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnviromentMove : MonoBehaviour
+{
+    public float Speed = 4f; //Ground: 4; Clouds: 0.1f
+
+    [SerializeField] float leftRange = -13f; //Ground: -38; Clouds: -44
+
+    Vector2 originalPosistion;
+
+    void Start()
+    {
+        originalPosistion = transform.position;
+    }
+
+    void Update()
+    {
+        if (transform.position.x >= leftRange)
+        {
+            transform.position = (Vector2)transform.position + Vector2.left * Time.deltaTime * Speed;
+        }
+        else
+        {
+            transform.position = originalPosistion;
+        }
+    }
+}
