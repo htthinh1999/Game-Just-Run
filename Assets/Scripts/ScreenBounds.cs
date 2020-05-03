c14e35d8f8bfd4c4a244b4fd455016e9a7d4afa5
htthinh1999 <htthinh1999@gmail.com>
2020-05-03 11:24:12 +0700

    Update Spawn Manager (Pool Object)

---
+++ Assets/Scripts/ScreenBounds.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenBounds: MonoBehaviour
+{
+    static Vector2 bounds;
+    public static float Left { get { return -bounds.x; } }
+    public static float Right { get { return bounds.x; } }
+    public static float Top { get { return bounds.y; } }
+    public static float Bottom { get { return -bounds.y; } }
+
+    void Start()
+    {
+        bounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+    }
+}
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public static SpawnManager Instance;
+
+    [SerializeField] float spawnDistance = 1.5f;
     [SerializeField] GameObject obstacle;
-    [SerializeField] int obstacleCount = 5;
+    [SerializeField] int obstacleCount = 50;
+    [SerializeField] float spawnDelayMin = 1;
+    [SerializeField] float spawnDelayMax = 3;
+
+    List<GameObject> poolObjects = new List<GameObject>();
+    float[] ySpawnPos;
+
+    void Awake()
+    {
+        Instance = this;
+        ySpawnPos = new float[] {transform.position.y + spawnDistance,
+                                transform.position.y,
+                                transform.position.y - spawnDistance};
+    }
 
     void Start()
     {
         SpawnObstacle(obstacleCount);
+        SpawnObstacleToScene();
     }
     
     void Update()
@@ -21,7 +38,54 @@ public class SpawnManager : MonoBehaviour
     {
         for(int i=0; i<count; i++)
         {
-            Instantiate(obstacle);
+            GameObject ob = Instantiate(obstacle, transform);
+            ob.SetActive(false);
+            poolObjects.Add(ob);
         }
     }
+
+    void SpawnObstacleToScene()
+    {
+        StartCoroutine(_SpawnObstacleToScene());
+    }
+
+    IEnumerator _SpawnObstacleToScene()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(spawnDelayMin, spawnDelayMax));
+            if (poolObjects.Count > 1)
+            {
+                for(int i=0; i<Random.Range(1, 3); i++) // Spawn 1 or 2 obstacle
+                {
+                    bool[] ySpawned = new bool[3];
+                    GameObject ob = poolObjects[0];
+
+                    // Spawn random position
+                    int index;
+                    do
+                    {
+                        index = Random.Range(0, 3);
+                    } while (ySpawned[index] == true);
+                    ySpawned[index] = true;
+
+                    ob.transform.position = new Vector2(ScreenBounds.Right, ySpawnPos[index]);
+                    ob.SetActive(true);
+                    poolObjects.Remove(ob);
+                }
+            }
+        }
+    }
+
+    public void ReturnToPool(GameObject obj)
+    {
+        if (!poolObjects.Contains(obj))
+        {
+            poolObjects.Add(obj);
+            obj.SetActive(false);
+        }
+    }
+
+
+
 }
